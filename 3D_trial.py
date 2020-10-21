5a18ad942f13390fe6f0624c052a60d5a7e110cd
Hong Huang <39689053+Geoffrey1500@users.noreply.github.com>
2020-10-21 15:05:50 +0800

    adding a new file

---
+++ 3D_trial.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KDTree
+import pickle
+from scipy.optimize import curve_fit
+from scipy.optimize import minimize
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(R)
+
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.viridis)
+
+plt.show()
